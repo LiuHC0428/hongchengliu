5fee5afec4ac82b680269c82498bfa3595c6ee40
LiuHC0428 <92698358+LiuHC0428@users.noreply.github.com>
2026-05-05 04:54:19 +0000

    Deploying to gh-pages from @ LiuHC0428/hongchengliu@9ef35041a5274476194fd0aca456c0f9ab7c08e7 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "LiuHC0428/hongcheng-s-CV",
+    "data-repo": "LiuHC0428/hongchengliu",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
